d1d942b03e8029fbace30528ba5123a75980c17f
Cameron Jensen <cameron@biteable.com>
2016-09-20 17:09:27 +1000

    buggered

--- app/image_generator.rb
+++ app/image_generator.rb
@@ -5,16 +5,17 @@ require 'ostruct'
 class ImageGenerator
 
   def initialize(args = {})
-    @args = args
-    @template = args.values_at(:template).first
-    @svg = get_svg()
+    @font_family = args.fetch(:font_family, 'Arial')
+    @font_color = args.fetch(:font_color, '#000')
+    @font_weight = args.fetch(:font_weight, 400)
+    @letter_spacing = args.fetch(:letter_spacing, 1)
+    @text = args.fetch(:text, 1)
+    @template = args.fetch(:template, 'left')
   end
 
   def get_svg()
     file = File.open("./templates/#{@template}.svg.erb").read
-    svg = ERB.new(file).result(OpenStruct.new(@args).instance_eval { binding })
-    puts svg
-    svg
+    svg = ERB.new(file).result(OpenStruct.new(self).instance_eval { binding })
   end
 
   def convert()
--- generate.rb
+++ generate.rb
@@ -1,5 +1,12 @@
 require './app/image_generator'
 
-image = ImageGenerator.new(:template => 'left', :font_family => 'Arial', :text => 'My text')
+image = ImageGenerator.new(
+  :template => 'left',
+  :font_color => '#222',
+  :font_family => 'Arial',
+  :font_weight => 400,
+  :letter_spacing => 3,
+  :text => 'My text'
+)
 output = image.convert()
 output.write('output/test.png')
